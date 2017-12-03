4ae29944345ad26fda28e7e94eb3ae06d7bfd4c0
Batiga Faouzane <fb@enyosolutions.com>
2017-12-03 20:00:59 +0000

    feat: added support for refMapping with a different target

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-module.exports = function dereference (methodsToHandle = ['find', 'findOne', 'findOneAndDelete', 'findOneAndUpdate']) {
-  return ({monkInstance}) => (next) => (args, method) => {
+module.exports = function dereference
+  (methodsToHandle = ['find', 'findOne', 'findOneAndDelete', 'findOneAndUpdate']) {
+  return ({ monkInstance }) => (next) => (args, method) => {
     return next(args, method).then((res) => {
       if (methodsToHandle.indexOf(method) === -1) {
         return res
@@ -17,7 +18,7 @@ module.exports = function dereference (methodsToHandle = ['find', 'findOne', 'fi
         if (!args.options.dereference[ref]) {
           return prev
         }
-        const collectionTarget = args.options.refMapping[ref]
+        var collectionTarget = args.options.refMapping[ref].collection || args.options.refMapping[ref]
         if (!collectionTarget) {
           console.error('Missing mapping for ref ' + ref)
           return prev
@@ -35,7 +36,7 @@ module.exports = function dereference (methodsToHandle = ['find', 'findOne', 'fi
           prev.push({
             ref,
             collectionTarget,
-            query: ids.length > 1 ? {$in: ids} : ids[0],
+            query: ids.length > 1 ? { $in: ids } : ids[0],
             fields: args.options.dereference[ref] === true ? undefined : args.options.dereference[ref]
           })
         }
@@ -47,25 +48,37 @@ module.exports = function dereference (methodsToHandle = ['find', 'findOne', 'fi
       }
 
       return Promise.all(queries.map(o => {
-        return monkInstance.get(o.collectionTarget).find({_id: o.query}, o.fields)
+        return monkInstance.get(o.collectionTarget).find({ _id: o.query }, o.fields)
       })).then((dereferences) => {
         if (isArrayResult) {
           res.forEach((o) => {
             dereferences.forEach((deref, i) => {
-              const {ref} = queries[i]
+              const { ref } = queries[i]
               if (!o[ref]) {
                 return
               }
-              o[ref] = deref.find((d) => monkInstance.id(d._id).toHexString() === monkInstance.id(o[ref]).toHexString()) || o[ref]
+              const value = deref.find((d) => monkInstance.id(d._id).toHexString() === monkInstance.id(o[ref]).toHexString()) || o[ref]
+
+              if (args.options.refMapping[ref].field) {
+                o[args.options.refMapping[ref].field] = value
+              } else {
+                o[ref] = value
+              }
             })
           })
         } else {
           dereferences.forEach((deref, i) => {
-            const {ref} = queries[i]
+            const { ref } = queries[i]
             if (!res[ref]) {
               return
             }
-            res[ref] = deref.find((d) => monkInstance.id(d._id).toHexString() === monkInstance.id(res[ref]).toHexString()) || res[ref]
+            const value = deref.find((d) => monkInstance.id(d._id).toHexString() === monkInstance.id(res[ref]).toHexString()) || res[ref]
+
+            if (args.options.refMapping[ref].field) {
+              res[args.options.refMapping[ref].field] = value
+            } else {
+              res[ref] = value
+            }
           })
         }
         return res
